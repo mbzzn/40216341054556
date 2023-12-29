4109b1349ba937e2fdd949a2f9101fd38566efab
mbzzn <155189506+mbzzn@users.noreply.github.com>
2023-12-29 11:51:57 -0800

    Add files via upload

--- tamrin2.cpp
+++ tamrin2.cpp
@@ -2,19 +2,17 @@
 using namespace std;
 int main()
 {
-int m,n,p,f,t;
-cin>>m>>n;
-f=m;
-p=n;
-do
-{
- t=m%n;
- m=n;
- n=t;
-}
-while(t!=0);
-// b,m,m;
-cout<<m<<endl;
-//k,m,m;
-cout<<(p*f)/m;
+long a,b,c;
+cin>>a;
+cin>>b;
+cin>>c;
+
+if(a>=b && a>=c)
+cout<<a<<endl;
+else
+    if(b>=a && b>=c)
+    cout<<b<<endl;
+else
+    cout<<c<<endl;
+return 0;
 }
